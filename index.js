9958f6c97c7186d975e4faf8a156e801a0e2acab
Alexander Pena <arpenacoding@gmail.com>
2023-01-08 22:48:05 -0800

    displaying files

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-fetch('Document_Metadata.json')
-  .then(response => response.text())
-  .then(data => {
-  	console.log(data);
-  })
-
-console.log('')
-var tag = document.createElement('p');
-var text = document.createTextNode("This is my text");
-tag.appendChild(text);
-document.body.appendChild(tag);
+fetch('documents/Document_Metadata.json')
+    .then(response => response.text())
+    .then(data => {
+        let jsonObj = JSON.parse(data)
+        jsonObj.forEach((element, idx) => {
+            let title = document.createTextNode(`File ${idx}`);
+            document.getElementById('json').appendChild(title);
+            let ul = document.createElement('ul')
+            for (let key in element) {
+                let entry = document.createElement('li');
+                let textNode = document.createTextNode(`${key}: ${element[key]}`);
+                entry.appendChild(textNode)
+                ul.appendChild(entry)
+            }
+            document.getElementById('json').appendChild(ul);
+        });
+    })
