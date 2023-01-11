e413d6239a3627811e476b1322d58b9616c37d63
Alexander Pena <arpenacoding@gmail.com>
2023-01-11 14:47:09 -0800

    added .prettierrc.json from other project

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 80,
+    semi: false,
+    arrowParens: 'avoid'
+  }
